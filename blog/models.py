fa6f112ce8b2beace8757d004d2e27b4572996b1
RYUCHUNGHYEON <ryu4714@gmail.com>
2022-09-20 22:33:22 +0900

    작성자 기능 구현

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 import os
 
 class Post(models.Model):
@@ -13,8 +14,9 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     # author 추후작성
 
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
-        return f"({self.pk})({self.title})"
+        return f"({self.pk})({self.title}) :: {self.author}"
 
     def get_absolute_url(self):
         return f"/blog/{self.pk}/"
