7c459a8fa5ecfdb13d9b42890cdba4ade252ffbe
RYUCHUNGHYEON <ryu4714@gmail.com>
2022-09-25 14:13:00 +0900

    카테고리 분류 페이지 구현

--- blog/views.py
+++ blog/views.py
@@ -15,16 +15,6 @@ def index(request):
     )
 """
 
-class PostList(ListView):
-    model = Post
-    ordering = "-pk"
-    template_name = "blog/index.html"
-    def get_context_data(self, **kwargs):
-        context = super(PostList, self).get_context_data()
-        context["categories"] = Category.objects.all()
-        context["no_category_post_count"] = Post.objects.filter(category=None).count()
-        return context
-
 """
 def post_detail(request, pk):
     post = Post.objects.get(pk=pk)
@@ -37,6 +27,37 @@ def post_detail(request, pk):
         }
     )
 """
+
+class PostList(ListView):
+    model = Post
+    ordering = "-pk"
+    template_name = "blog/index.html"
+    def get_context_data(self, **kwargs):
+        context = super(PostList, self).get_context_data()
+        context["categories"] = Category.objects.all()
+        context["no_category_post_count"] = Post.objects.filter(category=None).count()
+        return context
+
+
+def category_page(request, slug):
+    if slug == "no_category":
+        category = "미분류"
+        post_list = Post.objects.filter(category=None)
+    else:
+        category = Category.objects.get(slug=slug)
+        post_list = Post.objects.filter(category=category)
+
+    return render(
+        request,
+        "blog/index.html",
+        {
+            "post_list": post_list,
+            "categories": Category.objects.all(),
+            "no_category_post_count": Post.objects.filter(category=None).count(),
+            "category": category,
+        }
+    )
+
 
 class PostDetail(DetailView):
     model = Post
