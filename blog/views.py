5c799c171a66773fd43a3b44c1eab35df0beb7b7
RYUCHUNGHYEON <ryu4714@gmail.com>
2022-09-27 16:37:12 +0900

    포스트 작성 기능 구현 / 포스트 수정 기능 구현중

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from . models import Post, Category, Tag
+from django.core.exceptions import PermissionDenied
 # Create your views here.
 """
 def index(request):
@@ -80,3 +82,27 @@ def tag_page(requset, slug):
         }
     )
 
+class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+    model = Post
+    fields = ["title", "hook_text", "content", "head_image", "file_upload", "category"]
+
+    def test_func(self):
+        return self.request.user.is_superuser or self.request.user.is_staff
+
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated and (current_user.is_staff or current_user.is_superuser):
+            form.instance.author = current_user
+            return super(PostCreate, self).form_valid(form)
+        else:
+            return redirect("/blog")
+
+class PostUpdate(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ["title", "hook_text", "content", "head_image", "file_upload", "category", "tags"]
+
+    def dispatch(self, requset, *args, **kwargs):
+        if request.user.is_authenticated and requset.user == self.get_object().author:
+            return super(PostUpdate, self).dispatch(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
