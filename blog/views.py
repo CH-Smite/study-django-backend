ad5d9b6a748fb9a37049e0a1630e81efd98c301d
RYUCHUNGHYEON <ryu4714@gmail.com>
2022-10-01 23:58:58 +0900

    댓글 기능 작성 / 댓글 수정 기능 구현중

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from . models import Post, Category, Tag
+from .models import Post, Category, Tag, Comment
+from .forms import CommentForm
 from django.core.exceptions import PermissionDenied
 from django.utils.text import slugify
 # Create your views here.
@@ -47,6 +48,7 @@ class PostDetail(DetailView):
         context = super(PostDetail, self).get_context_data()
         context["categories"] = Category.objects.all()
         context["no_category_post_count"] = Post.objects.filter(category=None).count()
+        context["comment_form"] = CommentForm
         return context
 
 def category_page(request, slug):
@@ -158,3 +160,31 @@ class PostUpdate(LoginRequiredMixin, UpdateView):
 
         return context
 
+
+def new_comment(request, pk):
+    if request.user.is_authenticated:
+        post = get_object_or_404(Post, pk=pk)
+
+        if request.method =="POST":
+            comment_form = CommentForm(request.POST)
+            if comment_form.is_valid():
+                comment = comment_form.save(commit=False)
+                comment.post = post
+                comment.author = request.user
+                comment.save()
+                return redirect(comment.get_absolute_url())
+            else:
+                return redirect(post.get_absolute_url())
+        else:
+            raise PermissionDenied
+
+
+class CommentUpdate(LoginRequiredMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(CommentUpdate, self).dispatch(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
